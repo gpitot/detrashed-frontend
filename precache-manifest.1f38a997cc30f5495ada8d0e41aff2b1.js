021689ec39576cadb8e45999f17ba38d586abcc4
Guillaume Pitot <gpitot@nine.com.au>
2019-03-22 15:50:32 +1100

    Updates

---
+++ precache-manifest.1f38a997cc30f5495ada8d0e41aff2b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c930b72098fc0e2c06f",
+    "url": "/detrashed/static/css/main.4c854e86.chunk.css"
+  },
+  {
+    "revision": "3c930b72098fc0e2c06f",
+    "url": "/detrashed/static/js/main.417f8d1d.chunk.js"
+  },
+  {
+    "revision": "9fd4ad00e9b220db3816",
+    "url": "/detrashed/static/js/runtime~main.b590fe45.js"
+  },
+  {
+    "revision": "a3e9fef437a0b1428049",
+    "url": "/detrashed/static/js/2.66bfccb3.chunk.js"
+  },
+  {
+    "revision": "eceb9b0140714fa8169e364edda306f3",
+    "url": "/detrashed/index.html"
+  }
+];
