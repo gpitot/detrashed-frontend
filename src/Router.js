215dc346e37286d81e3ef91368a7deb4b6c74013
Guillaume Pitot <gpitot@nine.com.au>
2019-03-22 16:43:24 +1100

    404 page

--- src/Router.js
+++ src/Router.js
@@ -8,14 +8,15 @@ import Home from './common/pages/Home'
 import Test from './common/pages/Test/parent';
 
 export default function Router(props) {
+    console.log(process.env.PUBLIC_URL)
     return (
         <Switch>
 
-            <Route exact path="/" component={Home} />
-            <Route path="/callback" component={LoginCallbackPage} />
+            <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
+            <Route path={process.env.PUBLIC_URL + "/callback"} component={LoginCallbackPage} />
             
 
-            <Route path = "/test" component = {Test} />
+            <Route path = {process.env.PUBLIC_URL + "/test"} component = {Test} />
 
         </Switch>
     );
